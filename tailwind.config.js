3f29d1333863478dec9111e4f29a65ffcfc2fe02
Isha Gupta <isha.gupta@positrigo.com>
2025-02-08 10:46:03 -0800

    Revert "About page"
    This reverts commit 3cd337fa3c6b9945315517048b81973037eac6b9.

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,7 @@ export default {
   theme: {
     extend: {
       fontFamily: {
-        sans: ["Roboto", "sans-serif"],
+        sans: ["Inter", "sans-serif"], // Aesthetic font
       },
     },
   },
